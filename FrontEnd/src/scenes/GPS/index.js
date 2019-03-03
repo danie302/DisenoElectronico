6e866e148ce5b14cee3e0c68aa7c2a33ed451dc7
Daniel <ddazaj@uninorte.edu.co>
2019-03-03 11:44:48 -0500

    gps created

---
+++ FrontEnd/src/scenes/GPS/index.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class GPS extends Component {
+  render() {
+    return (
+      <div>
+        GPS
+      </div>
+    )
+  }
+}
+
+export default GPS
