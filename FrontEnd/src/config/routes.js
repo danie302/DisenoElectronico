33d27aeab844970d96c70a5f91a88322336945d0
Daniel <ddazaj@uninorte.edu.co>
2019-03-03 14:12:28 -0500

    gps visual updates

--- FrontEnd/src/config/routes.js
+++ FrontEnd/src/config/routes.js
@@ -5,15 +5,15 @@ import { Route, Switch } from "react-router-dom";
 // Components
 import App from "../scenes/App";
 import Home from "../scenes/Home";
-import GPS from "../scenes/GPS"
+import GPS from "../scenes/GPS";
 
 const AppRoutes = () => (
-  <App>
-    <Switch>
-      <Route path="/" exact component={Home} />
-      <Route path="/gps" exact component={GPS} />
-    </Switch>
-  </App>
+	<App>
+		<Switch>
+			<Route path="/" exact component={Home} />
+			<Route path="/gps" exact component={GPS} />
+		</Switch>
+	</App>
 );
 
 export default AppRoutes;
--- FrontEnd/src/scenes/GPS/index.js
+++ FrontEnd/src/scenes/GPS/index.js
@@ -1,43 +1,64 @@
 // Dependencies
-import React, { Component } from 'react'
-import { Map, Marker, GoogleApiWrapper } from 'google-maps-react'
+import React, { Component } from "react";
+import { Map, Marker, GoogleApiWrapper } from "google-maps-react";
+
+// Styles
+import "./gps.scss";
 
 export class GPS extends Component {
-    state = {
-        selectedPlace: "Hawai",
-        lat: 10.99304,
-        lon: -74.8281,
-        zoom: 18
-    }
-    onMouseMove(){
-        this.setState({
-            lon: this.state.lon + 0.0001,
-            lat: this.state.lat + 0.0001
-        })
-        console.log(this.state.lon);
-    }
+	state = {
+		selectedPlace: "Hawai",
+		lat: 10.99304,
+		lon: -74.8281,
+		zoom: 18
+	};
+	onMouseMove() {
+		this.setState({
+			lon: this.state.lon + 0.0001,
+			lat: this.state.lat + 0.0001
+		});
+		console.log(this.state.lon);
+	}
 
-  render() {
-    return (
-      <Map 
-      google={this.props.google}
-      zoom={this.state.zoom} 
-      onMousemove={this.onMouseMove.bind(this)}
-      initialCenter={{
-        lat: this.state.lat,
-        lng: this.state.lon
-      }} 
-      center= {{
-          lat: this.state.lat,
-          lng:this.state.lon
-      }}
-      >
-        <Marker onClick={this.onMarkerClick} name={'Current location'} position={{lat: this.state.lat, lng: this.state.lon}} />
-      </Map>
-    )
-  }
+	render() {
+		const style = {
+			width: "60%",
+			height: "60%",
+			position: "relative",
+			left: "25%"
+		};
+		return (
+			<div className="MapWrapper">
+				<div className="Box">
+					<input placeholder="ID del camion" class="Box--Search" />
+					<div className="Box--Button">Seguir</div>
+				</div>
+				<Map
+					className="Map"
+					style={style}
+					google={this.props.google}
+					zoom={this.state.zoom}
+					onMousemove={this.onMouseMove.bind(this)}
+					initialCenter={{
+						lat: this.state.lat,
+						lng: this.state.lon
+					}}
+					center={{
+						lat: this.state.lat,
+						lng: this.state.lon
+					}}
+				>
+					<Marker
+						onClick={this.onMarkerClick}
+						name={"Current location"}
+						position={{ lat: this.state.lat, lng: this.state.lon }}
+					/>
+				</Map>
+			</div>
+		);
+	}
 }
 
 export default GoogleApiWrapper({
-    apiKey: "AIzaSyAaUgFuVypDeWug-2htEXKEssI7TpifSg8"
-})(GPS)
+	apiKey: "AIzaSyAaUgFuVypDeWug-2htEXKEssI7TpifSg8"
+})(GPS);
