d92b65f74a7d2d52b20027ad58418960485a46ae
Daniel <ddazaj@uninorte.edu.co>
2019-03-03 14:48:22 -0500

    Contact page

--- FrontEnd/src/config/routes.js
+++ FrontEnd/src/config/routes.js
@@ -6,12 +6,14 @@ import { Route, Switch } from "react-router-dom";
 import App from "../scenes/App";
 import Home from "../scenes/Home";
 import GPS from "../scenes/GPS";
+import Contact from "../scenes/Contact";
 
 const AppRoutes = () => (
 	<App>
 		<Switch>
 			<Route path="/" exact component={Home} />
 			<Route path="/gps" exact component={GPS} />
+			<Route path="/contact" exact component={Contact} />
 		</Switch>
 	</App>
 );
