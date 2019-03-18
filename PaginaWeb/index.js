7ecb28a3b5a3f959510fecc4ab18f71748e21ef4
Daniel <ddazaj@uninorte.edu.co>
2019-03-18 09:23:43 -0500

    db update

--- PaginaWeb/index.js
+++ PaginaWeb/index.js
@@ -5,6 +5,7 @@ const path = require("path");
 const app = express();
 const gps = require("./config/routes");
 const gpsModel = require("./models/syrusTracking");
+const gpsConv = require("gpstime");
 let d;
 
 server.on("error", err => {
@@ -15,21 +16,6 @@ server.on("error", err => {
 server.on("message", (msg, rinfo) => {
 	console.log(`server got: ${msg} from ${rinfo.address}:${rinfo.port}`);
 	d = msg.toString("utf8");
-	if (d != null) {
-		let gpsData = {
-			id: null,
-			syrusID: "Avengers",
-			gpsTrama: msg
-		};
-		gpsModel.insertRaster(gpsData, function(error, data) {
-			//si el usuario se ha insertado correctamente mostramos su info
-			// if (data && data.insertId) {
-			// 	console.log("/users/" + data.insertId);
-			// } else {
-			// 	console.log({ msg: "Error" });
-			// }
-		});
-	}
 });
 
 server.on("listening", () => {
@@ -46,31 +32,50 @@ app.get("/map", function(req, res) {
 });
 
 app.get("/coord", (req, res) => {
-	let nWeeks, day, time, lon, lat, newMsg, data;
-	// >REV002041663724+1099304-0748281400000032;ID=AVENGERS<
-	let newData = d;
-	newMsg = newData.split("");
-	nWeeks = `${newMsg[6]}${newMsg[7]}${newMsg[8]}${newMsg[9]}`; // Number of weeks since 00:00 AM January 6, 1980
-	day = `${newMsg[10]}`; // Day of week. From 0 to 6 where 0 is Sunday.
-	time = `${newMsg[11]}${newMsg[12]}${newMsg[13]}${newMsg[14]}${newMsg[15]}`; // Time of the generated report. Seconds since 00:00 of the current date.
-	lat = `${newMsg[16]}${newMsg[17]}${newMsg[18]}.${newMsg[19]}${newMsg[20]}${
-		newMsg[21]
-	}${newMsg[22]}${newMsg[23]}`;
-	lon = `${newMsg[24]}${newMsg[25]}${newMsg[26]}${newMsg[27]}.${newMsg[28]}${
-		newMsg[29]
-	}${newMsg[30]}${newMsg[31]}${newMsg[32]}`;
-	//
-	data = {
-		msg: newMsg,
-		nWeeks: nWeeks,
-		day: day,
-		time: time,
-		lon: lon,
-		lat: lat
-	};
-	data = JSON.stringify(data);
-	console.log(data);
-	res.json(`${data}`);
+	if (d != null) {
+		let nWeeks, time, lon, lat, newMsg, data, ID;
+		// >REV002041663724+1099304-0748281400000032;ID=AVENGERS<
+		let newData = d;
+		newMsg = newData.split("");
+		nWeeks = `${newMsg[6]}${newMsg[7]}${newMsg[8]}${newMsg[9]}`; // Number of weeks since 00:00 AM January 6, 1980
+		time = `${newMsg[11]}${newMsg[12]}${newMsg[13]}${newMsg[14]}${newMsg[15]}`; // Time of the generated report. Seconds since 00:00 of the current date.
+		lat = `${newMsg[16]}${newMsg[17]}${newMsg[18]}.${newMsg[19]}${newMsg[20]}${
+			newMsg[21]
+		}${newMsg[22]}${newMsg[23]}`;
+		lon = `${newMsg[24]}${newMsg[25]}${newMsg[26]}${newMsg[27]}.${newMsg[28]}${
+			newMsg[29]
+		}${newMsg[30]}${newMsg[31]}${newMsg[32]}`;
+		ID = `${newMsg[45]}${newMsg[46]}${newMsg[47]}${newMsg[48]}${newMsg[49]}${
+			newMsg[50]
+		}${newMsg[51]}${newMsg[52]}`;
+		let date = gpsConv.wnTowToUtcTimestamp(nWeeks, parseFloat(time));
+		date = new Date(date);
+		dateString = date.toUTCString();
+		let gpsData = {
+			id: null,
+			date: dateString,
+			syrusID: ID,
+			lat: lat,
+			lon: lon
+		};
+		gpsModel.insertRaster(gpsData, function(error, data) {
+			console.log("Database Update");
+		});
+		//
+		data = {
+			msg: newMsg,
+			month: date.getUTCMonth(),
+			year: date.getUTCFullYear(),
+			day: date.getUTCDay(),
+			time: `${date.getUTCHours()}:${date.getUTCMinutes()}`,
+			lon: lon,
+			lat: lat
+		};
+		data = JSON.stringify(data);
+		res.json(`${data}`);
+	} else {
+		res.json("Error no data");
+	}
 });
 
 app.listen(4000, () => {
--- PaginaWeb/models/syrusTracking.js
+++ PaginaWeb/models/syrusTracking.js
@@ -14,7 +14,7 @@ let gpsModel = {};
 //obtenemos todos los usuarios
 gpsModel.getRasters = function(callback) {
 	if (connection) {
-		connection.query("SELECT * FROM `GPS-Tracking` ORDER BY id", function(
+		connection.query("SELECT * FROM `GPS-tracking` ORDER BY id", function(
 			error,
 			rows
 		) {
@@ -31,7 +31,7 @@ gpsModel.getRasters = function(callback) {
 gpsModel.getRaster = function(id, callback) {
 	if (connection) {
 		var sql =
-			"SELECT * FROM `GPS-Tracking` WHERE id = " + connection.escape(id);
+			"SELECT * FROM `GPS-tracking` WHERE id = " + connection.escape(id);
 		connection.query(sql, function(error, row) {
 			if (error) {
 				throw error;
@@ -46,7 +46,7 @@ gpsModel.getRaster = function(id, callback) {
 gpsModel.insertRaster = function(gpsData, callback) {
 	if (connection) {
 		// INSERT INTO `gps`.`GPS-Tracking` (`id`, `Syrus ID`, `GPS-Trama`) VALUES ('1', 'Avengers', '>REV002041663724+1099304-0748281400000032;ID=AVENGERS<');
-		connection.query("INSERT INTO `GPS-Tracking` SET ?", gpsData, function(
+		connection.query("INSERT INTO `GPS-tracking` SET ?", gpsData, function(
 			error,
 			result
 		) {
