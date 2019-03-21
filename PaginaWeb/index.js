7f84bc8efe39e0a4448c6b84dabeafc5a26e42bb
Daniel <ddazaj@uninorte.edu.co>
2019-03-21 15:28:25 -0500

    test

--- PaginaWeb/index.js
+++ PaginaWeb/index.js
@@ -16,12 +16,30 @@ server.on("message", (msg, rinfo) => {
 	console.log(`server got: ${msg} from ${rinfo.address}:${rinfo.port}`);
 	d = msg.toString("utf8");
 	if (d != null) {
-		let gpsData = {
+		let nWeeks, day, time, lon, lat, newMsg, data;
+		// >REV002041663724+1099304-0748281400000032;ID=AVENGERS<
+		let newData = d;
+		newMsg = newData.split("");
+		nWeeks = `${newMsg[6]}${newMsg[7]}${newMsg[8]}${newMsg[9]}`; // Number of weeks since 00:00 AM January 6, 1980
+		day = `${newMsg[10]}`; // Day of week. From 0 to 6 where 0 is Sunday.
+		time = `${newMsg[11]}${newMsg[12]}${newMsg[13]}${newMsg[14]}${newMsg[15]}`; // Time of the generated report. Seconds since 00:00 of the current date.
+		lat = `${newMsg[16]}${newMsg[17]}${newMsg[18]}.${newMsg[19]}${newMsg[20]}${
+			newMsg[21]
+		}${newMsg[22]}${newMsg[23]}`;
+		lon = `${newMsg[24]}${newMsg[25]}${newMsg[26]}${newMsg[27]}.${newMsg[28]}${
+			newMsg[29]
+		}${newMsg[30]}${newMsg[31]}${newMsg[32]}`;
+		//
+		data = {
 			id: null,
-			syrusID: "Avengers",
-			gpsTrama: msg
+			date: nWeeks,
+			time: time,
+			lon: lon,
+			lat: lat,
+			syrusID: "Avengers"
 		};
-		gpsModel.insertRaster(gpsData, function(error, data) {
+
+		gpsModel.insertRaster(data, function(error, data) {
 			//si el usuario se ha insertado correctamente mostramos su info
 			// if (data && data.insertId) {
 			// 	console.log("/users/" + data.insertId);
@@ -57,28 +75,6 @@ app.get("/hist", (req, res) => {
 });
 
 app.get("/coord", (req, res) => {
-	let nWeeks, day, time, lon, lat, newMsg, data;
-	// >REV002041663724+1099304-0748281400000032;ID=AVENGERS<
-	let newData = d;
-	newMsg = newData.split("");
-	nWeeks = `${newMsg[6]}${newMsg[7]}${newMsg[8]}${newMsg[9]}`; // Number of weeks since 00:00 AM January 6, 1980
-	day = `${newMsg[10]}`; // Day of week. From 0 to 6 where 0 is Sunday.
-	time = `${newMsg[11]}${newMsg[12]}${newMsg[13]}${newMsg[14]}${newMsg[15]}`; // Time of the generated report. Seconds since 00:00 of the current date.
-	lat = `${newMsg[16]}${newMsg[17]}${newMsg[18]}.${newMsg[19]}${newMsg[20]}${
-		newMsg[21]
-	}${newMsg[22]}${newMsg[23]}`;
-	lon = `${newMsg[24]}${newMsg[25]}${newMsg[26]}${newMsg[27]}.${newMsg[28]}${
-		newMsg[29]
-	}${newMsg[30]}${newMsg[31]}${newMsg[32]}`;
-	//
-	data = {
-		msg: newMsg,
-		nWeeks: nWeeks,
-		day: day,
-		time: time,
-		lon: lon,
-		lat: lat
-	};
 	data = JSON.stringify(data);
 	console.log(data);
 	res.json(`${data}`);
