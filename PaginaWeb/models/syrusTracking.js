d9a54db8dade58c65b9de54a9f88efd22283b030
carlos <ccarlosa@uninorte.edu.co>
2019-03-26 14:43:25 -0500

    historical

--- PaginaWeb/models/syrusTracking.js
+++ PaginaWeb/models/syrusTracking.js
@@ -1,79 +1,79 @@
 //llamamos al paquete mysql que hemos instalado
 var mysql = require("mysql"),
-	//creamos la conexion a nuestra base de datos con los datos de acceso de cada uno
-	connection = mysql.createConnection({
-		host: "avengers.cc2vb3ot4m30.us-east-1.rds.amazonaws.com",
-		user: "avenger",
-		password: "avenger0102",
-		database: "gps"
-	});
+  //creamos la conexion a nuestra base de datos con los datos de acceso de cada uno
+  connection = mysql.createConnection({
+    host: "avengers.cc2vb3ot4m30.us-east-1.rds.amazonaws.com",
+    user: "avenger",
+    password: "avenger0102",
+    database: "gps"
+  });
 
 //creamos un objeto para ir almacenando todo lo que necesitemos
 let gpsModel = {};
 
 //obtenemos todos los usuarios
 gpsModel.getRasters = function(callback) {
-	if (connection) {
-		connection.query("SELECT * FROM `GPS-tracking` ORDER BY id", function(
-			error,
-			rows
-		) {
-			if (error) {
-				throw error;
-			} else {
-				callback(null, rows);
-			}
-		});
-	}
+  if (connection) {
+    connection.query("SELECT * FROM `GPS-tracking` ORDER BY id", function(
+      error,
+      rows
+    ) {
+      if (error) {
+        throw error;
+      } else {
+        callback(null, rows);
+      }
+    });
+  }
 };
 
 //obtenemos un usuario por su id
 gpsModel.getRaster = function(id, callback) {
-	if (connection) {
-		var sql =
-			"SELECT * FROM `GPS-tracking` WHERE id = " + connection.escape(id);
-		connection.query(sql, function(error, row) {
-			if (error) {
-				throw error;
-			} else {
-				callback(null, row);
-			}
-		});
-	}
+  if (connection) {
+    var sql =
+      "SELECT * FROM `GPS-tracking` WHERE id = " + connection.escape(id);
+    connection.query(sql, function(error, row) {
+      if (error) {
+        throw error;
+      } else {
+        callback(null, row);
+      }
+    });
+  }
 };
 
 //añadir un nuevo usuario
 gpsModel.insertRaster = (gpsData, callback) => {
-	if (connection) {
-		// INSERT INTO `gps`.`GPS-Tracking` (`id`, `Syrus ID`, `GPS-Trama`) VALUES ('1', 'Avengers', '>REV002041663724+1099304-0748281400000032;ID=AVENGERS<');
-		connection.query("INSERT INTO `GPS-tracking` SET ?", gpsData, function(
-			error,
-			result
-		) {
-			if (error) {
-				throw error;
-			} else {
-				//devolvemos la última id insertada
-				callback(null, { insertId: result.insertId });
-			}
-		});
-	}
+  if (connection) {
+    // INSERT INTO `gps`.`GPS-Tracking` (`id`, `Syrus ID`, `GPS-Trama`) VALUES ('1', 'Avengers', '>REV002041663724+1099304-0748281400000032;ID=AVENGERS<');
+    connection.query("INSERT INTO `GPS-tracking` SET ?", gpsData, function(
+      error,
+      result
+    ) {
+      if (error) {
+        throw error;
+      } else {
+        //devolvemos la última id insertada
+        callback(null, { insertId: result.insertId });
+      }
+    });
+  }
 };
 gpsModel.getRange = (range, callback) => {
-	let { firstDate, lastDate, firstTime, lastTime } = range;
-	if (connection) {
-		connection.query(
-			"SELECT * FROM `GPS-tracking` WHERE date BETWEEN ? and ? and time BETWEEN ? and ?",
-			[firstDate, lastDate, firstTime, lastTime],
-			(error, rows) => {
-				if (error) {
-					throw error;
-				} else {
-					callback(null, rows);
-				}
-			}
-		);
-	}
+  let { firstDate, lastDate, firstTime, lastTime } = range;
+  if (connection) {
+    connection.query(
+      "SELECT * FROM `GPS-tracking` WHERE date BETWEEN ? and ? and time BETWEEN ? and ?",
+      [firstDate, lastDate, firstTime, lastTime],
+      (error, rows) => {
+        if (error) {
+          throw error;
+        } else {
+          callback(null, rows);
+        }
+      }
+    );
+  }
 };
 
 //exportamos el objeto para tenerlo disponible en la zona de rutas
