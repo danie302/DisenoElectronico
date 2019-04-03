6c8e206f174d0eee51157f6a99982b683dbd335e
Daniel <ddazaj@uninorte.edu.co>
2019-04-03 17:08:48 -0500

    fix bugs

--- PaginaWeb/public/track/historics/historics.js
+++ PaginaWeb/public/track/historics/historics.js
@@ -8,6 +8,7 @@ let lat = 10.99304;
 let lon = -74.8281;
 let flightPlanCoordinates = [];
 let flightPlanCoordinates2 = [];
+let safe = 0;
 
 function formatdate(date, time) {
 	let formDate = `${date.getUTCDate()}-${date.getUTCMonth() +
@@ -27,7 +28,13 @@ function initMap() {
 	};
 	// Creating the map
 	map = new google.maps.Map(document.getElementById("map"), opt);
-
+	let flightPath = new google.maps.Polyline({
+		path: [{ lat: 0, lng: 0 }, { lat: 0, lng: 0 }],
+		geodisc: true,
+		strokeColor: "#FF0000",
+		strokeOpacity: 1,
+		strokeWeight: 4
+	});
 	function drawRoad() {
 		let flightPath = new google.maps.Polyline({
 			path: flightPlanCoordinates,
@@ -89,30 +96,55 @@ function initMap() {
 		let nLdate = new Date(lDate.value);
 		let dateObj = formatdate(nFdate, fTime.value);
 		let dateObj2 = formatdate(nLdate, lTime.value);
-		let data = {
-			fDate: dateObj.formDate,
-			lDate: dateObj2.formDate,
-			fTime: dateObj.formTime,
-			lTime: dateObj2.formTime
-		};
-		axios
-			.post("/gps/hist", data)
-			.then(response => {
-				response.data.map((path, index) => {
-					flightPlanCoordinates[index] = {
-						lat: path.lat,
-						lng: path.lon
-					};
-					flightPlanCoordinates2[index] = {
-						time: path.time
-					};
+		let seconds1Date = nFdate.getTime() / 1000;
+		let seconds2Date = nLdate.getTime() / 1000;
+		let seconds1Hour = dateObj.formTime;
+		let seconds2Hour = dateObj2.formTime;
+		let a1 = seconds1Hour.split(":");
+		let b1 = seconds2Hour.split(":");
+		let sec1 = +a1[0] * 60 * 60 + +a1[1] * 60;
+		let sec2 = +b1[0] * 60 * 60 + +b1[1] * 60;
+		let secTot1 = sec1 + seconds1Date; // segundos totales de la fecha inicial incluyendo segundo de horas
+		let secTot2 = sec2 + seconds2Date; // segundos totales de la fecha final incluyendo segundo de horas
+
+		if (secTot1 < secTot2) {
+			let data = {
+				fDate: dateObj.formDate,
+				lDate: dateObj2.formDate,
+				fTime: dateObj.formTime,
+				lTime: dateObj2.formTime
+			};
+			axios
+				.post("/gps/hist", data)
+				.then(response => {
+					if (response.data.length == 0) {
+						safe = 1;
+					} else {
+						response.data.map((path, index) => {
+							flightPlanCoordinates[index] = {
+								lat: path.lat,
+								lng: path.lon
+							};
+							flightPlanCoordinates2[index] = {
+								time: path.time
+							};
+						});
+						safe = 0;
+					}
+				})
+				.then(() => {
+					if (safe == 0) {
+						drawRoad();
+					} else {
+						flightPlanCoordinates = [{ lat: 0, lng: 0 }];
+						alert("No data");
+					}
+				})
+				.catch(function(error) {
+					console.log(error);
 				});
-			})
-			.then(() => {
-				drawRoad();
-			})
-			.catch(function(error) {
-				console.log(error);
-			});
+		} else {
+			alert("Rango de fechas erroneas");
+		}
 	});
 }
