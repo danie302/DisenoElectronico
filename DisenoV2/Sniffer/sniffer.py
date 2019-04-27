8404cc76c2e6c8a80222cf54bd6510826d8be158
Daniel <ddazaj@uninorte.edu.co>
2019-04-27 10:08:43 -0500

    Sniffer finish

--- DisenoV2/Sniffer/sniffer.py
+++ DisenoV2/Sniffer/sniffer.py
@@ -13,17 +13,30 @@ sock.bind((UDP_IP, UDP_PORT))
 url = 'http://localhost:3000/api/v1/user/addLocation'
 headers = {'Content-Type': 'application/json'}
 
+# Message Structure
+# Hour:Min:Day:Month:Year:Lat:Lng:NumberOfRpm:TruckName
+# Message example
+# data = "14:30:5:4:2019:39.65765:-74.74267:841:Daza1"
 
 while True:
     data, addr = sock.recvfrom(1024) # buffer size is 1024 bytes
-    print (data.decode("utf-8"))
+    data = data.decode("utf-8")
+    print ("Received data:" + data)
+    data = data.split(sep=":")
+    time = data[0] + ":" + data[1]
+    date = data[2]+"-"+ data[3] + "-" + data[4]
+    lat = data[5]
+    lng = data[6]
+    rpm = data[7]
+    truckname = data[8]
     payload = {
-        'truckname': 'Daza1', 
-        'lat': 'value2', 
-        'lng': 'value2', 
-        'date': 'value2', 
-        'time': 'value2', 
-        'vel': 'value2', 
-        'rpm': 'value2'
-        }
+            'time': time, 
+            'date': date, 
+            'lat': lat, 
+            'lng': lng, 
+            'rpm': rpm,
+            'truckname': truckname 
+            }
+    print("Payload to send:")
+    print(payload)
     r = requests.post(url, headers=headers, data=json.dumps(payload))
