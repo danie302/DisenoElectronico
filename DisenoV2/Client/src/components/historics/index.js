e6c0ecfb0285cfba2598081457c64f5c164936f1
Daniel <ddazaj@uninorte.edu.co>
2019-05-11 14:12:16 -0500

    page fix

--- DisenoV2/Client/src/components/historics/index.js
+++ DisenoV2/Client/src/components/historics/index.js
@@ -19,21 +19,26 @@ class Historics extends Component {
 		super(props);
 		this.state = {
 			selectedTruck: null,
+			selectedTruck2: null,
 			startDate: new Date(),
 			endDate: new Date(),
+			startDate2: new Date(),
+			endDate2: new Date(),
 			mapCenter: {
 				lat: -3.745,
 				lng: -38.523
 			},
 			mapZoom: 7,
-			flightPath: [{ lat: -3.745, lng: -38.523 }]
+			flightPath: [{ lat: -3.745, lng: -38.523 }],
+			flightPathTruck2: [{ lat: -3.745, lng: -38.523 }]
 		};
 		this.onClick = this.onClick.bind(this);
+		this.onClick2 = this.onClick2.bind(this);
 		this.handleChangeStart = this.handleChangeStart.bind(this);
 		this.handleChangeEnd = this.handleChangeEnd.bind(this);
 		this.fetchHistorics = this.fetchHistorics.bind(this);
 		this.polylineClick = this.polylineClick.bind(this);
-		this.mapClick = this.mapClick.bind(this);
+		this.polylineClick2 = this.polylineClick2.bind(this);
 	}
 	componentDidMount() {
 		this.props.getUserTrucks();
@@ -41,6 +46,11 @@ class Historics extends Component {
 	onClick(truckname) {
 		this.setState({
 			selectedTruck: truckname
+		});
+	}
+	onClick2(truckname) {
+		this.setState({
+			selectedTruck2: truckname
 		});
 	}
 	handleChangeStart(date) {
@@ -91,10 +101,40 @@ class Historics extends Component {
 				mapZoom: 15
 			});
 		});
+		if (this.state.selectedTruck2 != null) {
+			console.log("Fetch second truck");
+
+			const data2 = {
+				truckname: this.state.selectedTruck2,
+				startDate: sDate,
+				startTime: sTime,
+				endDate: fDate,
+				endTime: fTime
+			};
+			axios.post("/api/v1/user/getLocations", data2).then(res => {
+				let locations = res.data.locations;
+				let paths = [];
+				let pathsTimes = [];
+				locations.map((path, index) => {
+					paths[index] = {
+						lat: parseFloat(path.Lat),
+						lng: parseFloat(path.Lng)
+					};
+					pathsTimes[index] = {
+						time: path.Time
+					};
+					return 0;
+				});
+				this.setState({
+					flightPathTruck2: paths,
+					flightPath2Truck2: pathsTimes,
+					coord2: paths[0]
+				});
+			});
+		}
 	}
 	polylineClick(point) {
 		let latlng = point.latLng;
-		console.log(latlng.lat);
 
 		let needle = {
 			minDistance: 9999999999,
@@ -105,8 +145,8 @@ class Historics extends Component {
 		};
 		this.state.flightPath.map((routePoint, index) => {
 			// http://www.movable-type.co.uk/scripts/latlong.html
-			const lat1 = latlng.lat;
-			const lon1 = latlng.lng;
+			const lat1 = latlng.lat();
+			const lon1 = latlng.lng();
 
 			const lat2 = routePoint.lat;
 			const lon2 = routePoint.lng;
@@ -136,8 +176,53 @@ class Historics extends Component {
 		});
 		alert("The truck was here at: " + needle.time + " and with: " + needle.rpm);
 	}
-	mapClick(args) {
-		console.log("onClick args: ", parseFloat(args));
+	polylineClick2(point) {
+		let latlng = point.latLng;
+
+		let needle = {
+			minDistance: 9999999999,
+			index: -1,
+			latlng: null,
+			time: null,
+			rpm: null
+		};
+		this.state.flightPathTruck2.map((routePoint, index) => {
+			// http://www.movable-type.co.uk/scripts/latlong.html
+			const lat1 = latlng.lat();
+			const lon1 = latlng.lng();
+
+			const lat2 = routePoint.lat;
+			const lon2 = routePoint.lng;
+
+			const R = 6371e3; // earth radius in meters
+			const φ1 = lat1 * (Math.PI / 180);
+			const φ2 = lat2 * (Math.PI / 180);
+			const Δφ = (lat2 - lat1) * (Math.PI / 180);
+			const Δλ = (lon2 - lon1) * (Math.PI / 180);
+
+			const a =
+				Math.sin(Δφ / 2) * Math.sin(Δφ / 2) +
+				Math.cos(φ1) * Math.cos(φ2) * (Math.sin(Δλ / 2) * Math.sin(Δλ / 2));
+
+			const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+
+			let dist = R * c;
+
+			if (dist < needle.minDistance) {
+				needle.minDistance = dist;
+				needle.index = index;
+				needle.latlng = routePoint;
+				needle.time = this.state.flightPath2Truck2[index].time;
+				needle.rpm = this.state.flightPath2Truck2[index].rpm;
+			}
+			return 0;
+		});
+		alert(
+			"The second truck was here at: " +
+				needle.time +
+				" and with: " +
+				needle.rpm
+		);
 	}
 
 	render() {
@@ -145,59 +230,188 @@ class Historics extends Component {
 		const { trucks } = this.props.profile;
 		const { selectedTruck } = this.state;
 		let map;
+		let drop1, drop2;
+		drop1 = (
+			<div className='dropdown'>
+				<button
+					className='btn btn-secondary dropdown-toggle'
+					type='button'
+					id='dropdownMenuButton'
+					data-toggle='dropdown'
+					aria-haspopup='true'
+					aria-expanded='false'
+				>
+					{this.state.selectedTruck
+						? " " + this.state.selectedTruck + " "
+						: " Truck "}
+				</button>
+				<div className='dropdown-menu' aria-labelledby='dropdownMenuButton'>
+					{trucks.map(truck => {
+						return (
+							<div
+								className='dropdown-item'
+								key={truck.id}
+								type='button'
+								onClick={() => {
+									this.onClick(truck.truckname);
+								}}
+							>
+								{truck.truckname}
+							</div>
+						);
+					})}
+				</div>
+			</div>
+		);
 		if (selectedTruck == null) {
 			map = <div />;
 		} else {
-			map = (
-				<div>
-					<LoadScript
-						id='script-loader'
-						googleMapsApiKey='AIzaSyAaUgFuVypDeWug-2htEXKEssI7TpifSg8'
+			drop2 = (
+				<div className='dropdown'>
+					<button
+						className='btn btn-secondary dropdown-toggle'
+						type='button'
+						id='dropdownMenuButton'
+						data-toggle='dropdown'
+						aria-haspopup='true'
+						aria-expanded='false'
 					>
-						<GoogleMap
-							id='map'
-							mapContainerStyle={{
-								height: "400px",
-								width: "800px",
-								borderRadius: "10px",
-								margin: "4px"
-							}}
-							onClick={e => {
-								this.mapClick(e.latLng);
-							}}
-							zoom={this.state.mapZoom}
-							center={this.state.mapCenter}
-						>
-							<Polyline
-								path={this.state.flightPath}
-								options={{
-									strokeColor: "#FF0000",
-									strokeOpacity: 0.8,
-									strokeWeight: 3,
-									fillColor: "#FF0000",
-									fillOpacity: 0.35,
-									clickable: true,
-									draggable: false,
-									editable: false,
-									visible: true,
-									radius: 300,
-									zIndex: 1
-								}}
-								onClick={point => {
-									this.polylineClick(point);
-									console.log(point);
-								}}
-							/>
-						</GoogleMap>
-					</LoadScript>
-					<div
-						className='btn btn-info btn-block mt-4'
-						onClick={this.fetchHistorics}
-					>
-						Show data
+						{this.state.selectedTruck2
+							? " " + this.state.selectedTruck2 + " "
+							: " Truck "}
+					</button>
+					<div className='dropdown-menu' aria-labelledby='dropdownMenuButton'>
+						{trucks.map(truck => {
+							return (
+								<div
+									className='dropdown-item'
+									key={truck.id}
+									type='button'
+									onClick={() => {
+										this.onClick2(truck.truckname);
+									}}
+								>
+									{truck.truckname}
+								</div>
+							);
+						})}
 					</div>
 				</div>
 			);
+			if (this.state.selectedTruck2 == null) {
+				map = (
+					<div>
+						<LoadScript
+							id='script-loader'
+							googleMapsApiKey='AIzaSyAaUgFuVypDeWug-2htEXKEssI7TpifSg8'
+						>
+							<GoogleMap
+								id='map'
+								mapContainerStyle={{
+									height: "40vw",
+									width: "80vw",
+									borderRadius: "10px",
+									margin: "4px"
+								}}
+								zoom={this.state.mapZoom}
+								center={this.state.mapCenter}
+							>
+								<Polyline
+									path={this.state.flightPath}
+									options={{
+										strokeColor: "#FF0000",
+										strokeOpacity: 0.8,
+										strokeWeight: 3,
+										fillColor: "#FF0000",
+										fillOpacity: 0.35,
+										clickable: true,
+										draggable: false,
+										editable: false,
+										visible: true,
+										radius: 300,
+										zIndex: 1
+									}}
+									onClick={point => {
+										this.polylineClick(point);
+									}}
+								/>
+							</GoogleMap>
+						</LoadScript>
+						<div
+							className='btn btn-info btn-block mt-4'
+							onClick={this.fetchHistorics}
+						>
+							Show data
+						</div>
+					</div>
+				);
+			} else {
+				map = (
+					<div>
+						<LoadScript
+							id='script-loader'
+							googleMapsApiKey='AIzaSyAaUgFuVypDeWug-2htEXKEssI7TpifSg8'
+						>
+							<GoogleMap
+								id='map'
+								mapContainerStyle={{
+									height: "40vw",
+									width: "80vw",
+									borderRadius: "10px",
+									margin: "4px"
+								}}
+								zoom={this.state.mapZoom}
+								center={this.state.mapCenter}
+							>
+								<Polyline
+									path={this.state.flightPath}
+									options={{
+										strokeColor: "#FF0000",
+										strokeOpacity: 0.8,
+										strokeWeight: 6,
+										fillColor: "#FF0000",
+										fillOpacity: 0.35,
+										clickable: true,
+										draggable: false,
+										editable: false,
+										visible: true,
+										radius: 300,
+										zIndex: 1
+									}}
+									onClick={point => {
+										this.polylineClick(point);
+									}}
+								/>
+								<Polyline
+									path={this.state.flightPathTruck2}
+									options={{
+										strokeColor: "#1da30b",
+										strokeOpacity: 0.8,
+										strokeWeight: 3,
+										fillColor: "#1da30b",
+										fillOpacity: 0.35,
+										clickable: true,
+										draggable: false,
+										editable: false,
+										visible: true,
+										radius: 300,
+										zIndex: 1
+									}}
+									onClick={point => {
+										this.polylineClick2(point);
+									}}
+								/>
+							</GoogleMap>
+						</LoadScript>
+						<div
+							className='btn btn-info btn-block mt-4'
+							onClick={this.fetchHistorics}
+						>
+							Show data
+						</div>
+					</div>
+				);
+			}
 		}
 		return (
 			<div className=''>
@@ -208,37 +422,10 @@ class Historics extends Component {
 							<div>
 								<p className=' lead text-muted'>Welcome {user.user}</p>
 								<p>Choose the truck you want to see the historics</p>
-								<div className='dropdown'>
-									<button
-										className='btn btn-secondary dropdown-toggle'
-										type='button'
-										id='dropdownMenuButton'
-										data-toggle='dropdown'
-										aria-haspopup='true'
-										aria-expanded='false'
-									>
-										{this.state.selectedTruck
-											? " " + this.state.selectedTruck + " "
-											: " Truck "}
-									</button>
-									<div
-										className='dropdown-menu'
-										aria-labelledby='dropdownMenuButton'
-									>
-										{trucks.map(truck => {
-											return (
-												<div
-													className='dropdown-item'
-													key={truck.id}
-													type='button'
-													onClick={() => {
-														this.onClick(truck.truckname);
-													}}
-												>
-													{truck.truckname}
-												</div>
-											);
-										})}
+								<div className='container'>
+									<div className='row'>
+										<div className='col'>{drop1}</div>
+										<div className='col'>{drop2}</div>
 									</div>
 								</div>
 								<div>Date Picker</div>
