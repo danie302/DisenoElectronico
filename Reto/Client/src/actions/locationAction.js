3519b2cec9b73c55ee25420ea2bb3d636e05ba4a
Daniel <ddazaj@uninorte.edu.co>
2019-05-28 00:34:35 -0500

    fix cors reto

--- Reto/Client/src/actions/locationAction.js
+++ Reto/Client/src/actions/locationAction.js
@@ -2,7 +2,9 @@
 import { GET_LOCATIONS } from "./types.js";
 
 export const getLocations = () => dispatch => {
-	fetch("http://localhost:4000/api/calls")
+	fetch("http://localhost:4000/api/calls", {
+		headers: { "Access-Control-Allow-Origin": "http://34.197.229.47:4000/" }
+	})
 		.then(res => {
 			return res.json();
 		})
@@ -19,7 +21,10 @@ export const deleteLocation = payload => dispatch => {
 
 	fetch("http://localhost:4000/api/deleteCall", {
 		method: "POST",
-		headers: { "Content-Type": "application/json" },
+		headers: {
+			"Content-Type": "application/json",
+			"Access-Control-Allow-Origin": "http://34.197.229.47:4000/"
+		},
 		body: JSON.stringify({ payload })
 	});
 };
