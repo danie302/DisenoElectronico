8a1a8506a56bd2f1d733df56d93e7c2b82c66a53
Daniel <ddazaj@uninorte.edu.co>
2019-05-29 23:18:39 -0500

    update2

--- Reto/Client/src/actions/locationAction.js
+++ Reto/Client/src/actions/locationAction.js
@@ -2,8 +2,8 @@
 import { GET_LOCATIONS } from "./types.js";
 
 export const getLocations = () => dispatch => {
-	fetch("http://localhost:4000/api/calls", {
-		headers: { "Access-Control-Allow-Origin": "localhost:4000/" }
+	fetch("http://34.197.229.47:4000/api/calls", {
+		headers: { "Access-Control-Allow-Origin": "34.197.229.47:4000/" }
 	})
 		.then(res => {
 			return res.json();
@@ -19,11 +19,11 @@ export const getLocations = () => dispatch => {
 export const deleteLocation = payload => dispatch => {
 	console.log(payload);
 
-	fetch("http://localhost:4000/api/deleteCall", {
+	fetch("http://34.197.229.47:4000/api/deleteCall", {
 		method: "POST",
 		headers: {
 			"Content-Type": "application/json",
-			"Access-Control-Allow-Origin": "localhost:4000/"
+			"Access-Control-Allow-Origin": "34.197.229.47:4000/"
 		},
 		body: JSON.stringify({ payload })
 	});
